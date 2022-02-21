930e1e234fa5309269b95a6a3b5c995c3bb60c2d
Rez5dev <99760807+Rez5dev@users.noreply.github.com>
2022-02-21 21:29:21 +0600

    albann

---
+++ rollup.config.js
@@ -0,0 +1,115 @@
+import path from 'path'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import { uglify } from 'rollup-plugin-uglify'
+import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
+
+const root = process.platform === 'win32' ? path.resolve('/') : '/'
+const external = id => !id.startsWith('.') && !id.startsWith(root)
+const extensions = ['.js', '.jsx', '.ts', '.tsx']
+const getBabelOptions = ({ useESModules }, targets) => ({
+  babelrc: false,
+  extensions,
+  exclude: '**/node_modules/**',
+  runtimeHelpers: true,
+  presets: [
+    ['@babel/preset-env', { loose: true, modules: false, targets }],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    ['@babel/proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
+    ['transform-react-remove-prop-types', { removeImport: true }],
+    ['@babel/transform-runtime', { regenerator: false, useESModules }],
+  ],
+})
+
+function createConfig(entry, out) {
+  return [
+    {
+      input: `./src/${entry}/index`,
+      output: { file: `dist/${out}.js`, format: 'esm' },
+      external,
+      plugins: [
+        babel(
+          getBabelOptions(
+            { useESModules: true },
+            '>1%, not dead, not ie 11, not op_mini all'
+          )
+        ),
+        sizeSnapshot(),
+        resolve({ extensions }),
+      ],
+    },
+    {
+      input: `./src/${entry}/index`,
+      output: { file: `dist/${out}.cjs.js`, format: 'cjs' },
+      external,
+      plugins: [
+        babel(getBabelOptions({ useESModules: false })),
+        sizeSnapshot(),
+        resolve({ extensions }),
+      ],
+    },
+  ]
+}
+
+function createCjs(entry, out) {
+  return [
+    {
+      input: `./src/${entry}/index`,
+      output: { file: `dist/${out}.js`, format: 'cjs' },
+      external,
+      plugins: [
+        babel(
+          getBabelOptions(
+            { useESModules: true },
+            '>1%, not dead, not ie 11, not op_mini all'
+          )
+        ),
+        sizeSnapshot(),
+        resolve({ extensions }),
+      ],
+    },
+    {
+      input: `./src/${entry}/index`,
+      output: { file: `dist/${out}.cjs.js`, format: 'cjs' },
+      external,
+      plugins: [
+        babel(getBabelOptions({ useESModules: false })),
+        sizeSnapshot(),
+        resolve({ extensions }),
+      ],
+    },
+  ]
+}
+
+export default [
+  ...createConfig('targets/web', 'web'),
+  ...createConfig('targets/native', 'native'),
+  ...createConfig('targets/universal', 'universal'),
+  ...createConfig('targets/konva', 'konva'),
+  ...createConfig('targets/three', 'three'),
+  ...createConfig('targets/zdog', 'zdog'),
+  {
+    input: `./src/targets/cookbook/index`,
+    output: { file: `dist/cookbook.js`, format: 'esm' },
+    external,
+    plugins: [
+      babel(
+        getBabelOptions(
+          { useESModules: true },
+          '>1%, not dead, not ie 11, not op_mini all'
+        )
+      ),
+      resolve({ extensions }),
+    ],
+  },
+  ...createCjs('renderprops/targets/web', 'renderprops'),
+  ...createCjs('renderprops/addons', 'renderprops-addons'),
+  ...createCjs('renderprops/targets/native', 'renderprops-native'),
+  ...createCjs('renderprops/targets/universal', 'renderprops-universal'),
+  ...createCjs('renderprops/targets/konva', 'renderprops-konva'),
+]
---
+++ src/useTrail.js
@@ -0,0 +1,55 @@
+import { useRef, useState, useMemo, useEffect } from 'react'
+import { callProp, is } from './shared/helpers'
+import { useSprings } from './useSprings'
+
+/** API
+ * const trails = useTrail(number, { ... })
+ * const [trails, set] = useTrail(number, (number) => ({ ... }))
+ */
+
+export const useTrail = (length, props) => {
+  const mounted = useRef(false)
+  const isFn = is.fun(props)
+  const instances = useRef()
+  const currentProps = useRef()
+  const [reverse, setReverse] = useState(false)
+
+  const [result, set, pause] = useSprings(length, (i, ctrl) => {
+    const updateProps = callProp(props, i)
+    if (i === 0) {
+      instances.current = []
+      currentProps.current = []
+      setReverse(updateProps.reverse)
+    }
+    instances.current.push(ctrl)
+    currentProps.current.push(updateProps)
+    return {
+      ...updateProps,
+      config: callProp(updateProps.config, i),
+      attach: i > 0 && (() => instances.current[i - 1]),
+    }
+  })
+
+  // Set up function to update controller
+  const updateCtrl = useMemo(
+    () => props =>
+      set((i, ctrl) => {
+        const updateProps = currentProps.current[i]
+        const last = props.reverse ? i === 0 : length - 1 === i
+        const attachIdx = props.reverse ? i + 1 : i - 1
+        const attachController = instances.current[attachIdx]
+        return {
+          ...props,
+          config: callProp(props.config || updateProps.config, i),
+          attach: attachController && (() => attachController),
+        }
+      }),
+    [length, reverse]
+  )
+  // Update controller if props aren't functional
+  useEffect(() => void (mounted.current && !isFn && updateCtrl(props)))
+  // Update mounted flag and destroy controller on unmount
+  useEffect(() => void (mounted.current = true), [])
+
+  return isFn ? [result, updateCtrl, pause] : result
+}
